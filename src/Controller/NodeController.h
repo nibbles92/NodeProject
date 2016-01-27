cbc8958442d33d2b05d4276d5a7fd363393b9d7d
nibbles92 <droc6148@740s-pl-117.local>
2016-01-27 13:05:14 -0700

    started Node

---
+++ src/Controller/NodeController.h
@@ -0,0 +1,18 @@
+/*
+ * NodeController.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: droc6148
+ */
+
+#ifndef CONTROLLER_NODECONTROLLER_H_
+#define CONTROLLER_NODECONTROLLER_H_
+
+class NodeController {
+public:
+	NodeController();
+	virtual ~NodeController();
+	void start();
+};
+
+#endif /* CONTROLLER_NODECONTROLLER_H_ */
